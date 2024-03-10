7b46d728642e598c1179474592e0b54fff51cee6
swamfire <usern5949@gmail.com>
2024-03-10 20:34:34 +0530

    Added generics.CreateAPIView

--- backend/products/views.py
+++ backend/products/views.py
@@ -3,6 +3,19 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        print(serializer.validated_data)
+        title = serializer.validated_data.get("title")
+        content = serializer.validated_data.get("content") or None
+        if(content is None):
+            content = title
+        serializer.save(content=content)
+
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
