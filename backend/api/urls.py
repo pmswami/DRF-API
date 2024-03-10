efa17b34a739bc483971381747bfd7f005f4a67e
swamfire <usern5949@gmail.com>
2024-03-10 20:09:41 +0530

    Added generics.RetrieveAPIView

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path("", views.api_home),
+    # path("products/", include("products.urls"))
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Product
+from .serializers import ProductSerializer
+
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_field = "pk"
