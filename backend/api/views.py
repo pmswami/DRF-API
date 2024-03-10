831ab0d0ad8c803bf2d042d71baabbbb218942a9
swamfire <usern5949@gmail.com>
2024-03-10 16:48:54 +0530

    Created basic API

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
 # Create your views here.
+def api_home(request, *args, **kwargs):
+    body = request.body
+    # print(body)
+    try:
+        data = json.loads(body)
+        # print(data)
+        params = request.GET
+        print(params)
+    except Exception as error:
+        return JsonResponse({"error": error})
+    # return JsonResponse({"msg": "Hi there! This is your Django API response !"})
+    # return JsonResponse({"msg": "Hi there! This is your Django API response !"})
+    data["headers"] = dict(request.headers)
+    data["content_type"] = request.content_type
+    data["params"] = dict(request.GET)
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,14 +2,18 @@ import requests
 
 # endpoint = f"https://httpbin.org/"
 # endpoint = f"https://httpbin.org/anything"
-endpoint = "http://127.0.0.1:8000/"
+# endpoint = "http://127.0.0.1:8000/"
+endpoint = "http://127.0.0.1:8000/api/"
 
 # response = requests.get(endpoint, json={"msg":"Hello Wolrd"})
-response = requests.get(endpoint, data={"msg":"Hello World"})
+# response = requests.get(endpoint, data={"msg":"Hello World"})
+
+# response = requests.get(endpoint, params={"abc":123},json={"query": "Hello world"}, data={"msg":"Hello World"})
+response = requests.get(endpoint, params={"abc":123},json={"query ": "Hello world"})
 # print(response.text)
 
 # JSON => JavaScript Object Notation
 # Very much similar to Python Dict
 
-print("json_response" ,response.json())
-print("response status code" ,response.status_code)
+print("json_response - " ,response.json())
+print("response status code - " ,response.status_code)
