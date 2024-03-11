9876e25b454865412100c6afdae83b347338d07e
swamfire <usern5949@gmail.com>
2024-03-11 10:12:38 +0530

    Added generics.DestroyAPIView, generics.UpdateAPIView

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.ProductListCreateAPIView.as_view()),
-    path("<int:pk>", views.ProductDetailAPIView.as_view())
+    path("<int:pk>", views.ProductDetailAPIView.as_view()),
+    path("<int:pk>/update/", views.ProductUpdateAPIView.as_view()),
+    path("<int:pk>/delete/", views.ProductDeleteAPIView.as_view())
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+# from django.http import Http404
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -13,16 +17,60 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         content = serializer.validated_data.get("content") or None
         if(content is None):
             content = title
-        serializer.save(content=content)
-
-
+        instance = serializer.save(content=content)
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field = "pk"
 
-class ProductListAPIView(generics.ListAPIView):
+class ProductUpdateAPIView(generics.UpdateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    # lookup_field = "pk"
+    lookup_field = "pk"
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+
+class ProductDeleteAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = "pk"
+
+    def perform_destroy(self, instance):
+        super().perform_destroy(instance)
+
+
+# class ProductListAPIView(generics.ListAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
+#     # lookup_field = "pk"
+
+@api_view(["GET", "POST"])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+    if(method=="GET"):
+        #Details view
+        #List view
+        if pk is not None:
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True).data
+        return Response(serializer)
+
+    if(method == "POST"):
+        #Create new item
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            # print(serializer.data)
+            title = serializer.validated_data.get("title")
+            content = serializer.validated_data.get("content") or None
+            if(content is None):
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid": "not good data"})
