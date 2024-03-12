0f032c227fd6733b344f0ac043959634c23046c7
swamfire <usern5949@gmail.com>
2024-03-12 11:21:56 +0530

    Added validators for serializers

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
+
 from .models import Product
+from .validators import validate_title, validate_title_no_hello, unique_product_title
 
 class ProductSerializer(serializers.ModelSerializer):
     discount = serializers.SerializerMethodField(read_only=True)
@@ -9,11 +11,20 @@ class ProductSerializer(serializers.ModelSerializer):
     #     view_name="product-detail",
     #     lookup_field = "pk"
     # )
-    email = serializers.EmailField(write_only=True)
-
+    # email = serializers.EmailField(write_only=True)
+    # title = serializers.CharField(validators=[validate_title])
+    title = serializers.CharField(validators=[validate_title_no_hello, unique_product_title])
+    # name = serializers.CharField(source="title", read_only=True)
     class Meta:
         model = Product
-        fields = ['pk', "title", 'price', "sale_price", "discount", "email"]
+        fields = [
+            'pk',
+            # "name",
+            "title",
+            'price',
+            "sale_price",
+            "discount"
+        ]
 
     # def get_url(self, obj):
     #     # return f"/api/products/{obj.pk}/"
@@ -35,6 +46,17 @@ class ProductSerializer(serializers.ModelSerializer):
     # def update(self, instance, validated_data):
     #     instance.title=validated_data.get("title")
     #     return instance
+
+    # Validate Title
+    # def validate_title(self, value):
+    #     # qs = Product.objects.filter(title__exact=value)
+    #     # qs = Product.objects.filter(title__iexact=value) # for case insensitive comparison
+    #     request = self.context.get("request")
+    #     user = request.user
+    #     qs = Product.objects.filter(user=user, title__iexact=value) # for case insensitive comparison
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name.")
+    #     return value
 
     def get_discount(self, obj):
         try:
---
+++ backend/products/validators.py
@@ -0,0 +1,19 @@
+
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+
+from .models import Product
+
+def validate_title(value):
+        # qs = Product.objects.filter(title__exact=value)
+        qs = Product.objects.filter(title__iexact=value) # for case insensitive comparison
+        if qs.exists():
+            raise serializers.ValidationError(f"{value} is already a product name.")
+        return value
+
+def validate_title_no_hello(value):
+      if "hello" in value.lower():
+            raise serializers.ValidationError("Hello is not allowed")
+      return value
+
+unique_product_title = UniqueValidator(queryset=Product.objects.all())
